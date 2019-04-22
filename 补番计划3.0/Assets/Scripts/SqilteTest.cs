7b9f4332746101674a119cc105bc378e4d321f84
lihua <poinseting@163.com>
2019-04-22 16:12:23 +0800

    添加Sqlite插件

---
+++ 补番计划3.0/Assets/Scripts/SqilteTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SqilteTest : MonoBehaviour
+{
+	// https://www.jianshu.com/p/381ed9d59654
+
+	public SqliteDatabase Sqlite;
+
+	void Start()
+    {
+		//打开测试数据库
+		Sqlite = new SqliteDatabase("test.db");
+
+		//添加数据
+		Sqlite.ExecuteNonQuery("insert into example (name) values('test name')");
+		PrintData();
+
+	}
+
+   private void PrintData()
+	{
+		DataTable dt = Sqlite.ExecuteQuery("SELECT * FROM example");
+		string name;
+		int id;
+		foreach(DataRow item in dt.Rows)
+		{
+			id = (int)item["id"];
+			name = (string)item["name"];
+			Debug.Log(id + "  " + name);
+		}
+	}
+}
